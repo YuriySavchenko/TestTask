e58d6fa26304e9cb342e7dd2c7e8b486ca0aaff8
YuriySavchenko <yurec.proger@gmail.com>
2018-10-20 23:52:27 +0300

    struct for saving intervals

---
+++ src/headers/Interval.h
@@ -0,0 +1,15 @@
+//
+// Created by yuriy on 10/20/18.
+//
+
+#ifndef TEST_WORK_INTERVAL_H
+#define TEST_WORK_INTERVAL_H
+
+
+struct Interval {
+    int low;
+    int high;
+};
+
+
+#endif //TEST_WORK_INTERVAL_H
