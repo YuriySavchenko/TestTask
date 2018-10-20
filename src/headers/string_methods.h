7c2f559ac839a6ee8f80315096e19b73b4a3bd8d
YuriySavchenko <yurec.proger@gmail.com>
2018-10-20 23:53:38 +0300

    header file for work with string

---
+++ src/headers/string_methods.h
@@ -0,0 +1,22 @@
+//
+// Created by yuriy on 10/20/18.
+//
+
+#ifndef TEST_WORK_STRING_METHODS_H
+#define TEST_WORK_STRING_METHODS_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string digitString(std::string);
+
+std::string pureString(std::string);
+
+int indexString(int, int, char, std::string);
+
+bool cmpString(std::string, std::string);
+
+std::vector<std::string> splitString(std::string, char);
+
+#endif //TEST_WORK_STRING_METHODS_H
